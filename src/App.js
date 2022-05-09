60b266698a96ee82c89d1382aa6751ba8b7ab614
SadnEmoBot <August_Duu@163.com>
2022-05-09 15:34:15 +0800

    feat: 配置home和navigation的父子路由，样式，logo(svg)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import CategoryList from "./components/category-list/category-list";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 const App = () => {
-    return <CategoryList />;
+    return (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
---
+++ src/routes/home/home.jsx
@@ -0,0 +1,7 @@
+import CategoryList from "../../components/category-list/category-list";
+
+const home = () => {
+    return <CategoryList />;
+};
+
+export default home;
